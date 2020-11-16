c7bfcfcc6f72d96136628c37158c58cddb6a1d06
Andy Park <apark1214@gmail.com>
2020-11-16 15:19:34 -0800

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+
+
+var width =5;
+var height =4;
+
+var area = width * height;
+console.log('the value of the area:', area);
+console.log('typeof area:', typeof area);
+
+
+var bill=20;
+var payment =100;
+
+var change = payment-bill;
+console.log('the value of the change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes =80;
+var midterm = 90;
+var final =96;
+
+var grade = (quizzes+midterm+final)/3
+console.log('the value of the grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName ='Jason';
+var lastName="Smith";
+
+var fullName = firstName + " " + lastName;
+console.log('the value of full name:', fullName);
+console.log('typeof full name:', typeof fullName);
+
+var ph =8;
+var isAcidic = ph <7
+
+console.log('the value of is Acidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount =70;
+var isSparta = headCount === 300;
+console.log('the value of is Sparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto += ' is the GOAT';
+console.log('the value of motto:', motto);
+console.log('typeof motto:', typeof motto);
