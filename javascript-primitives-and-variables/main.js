a09f6eec5a9d62bd6d521fbeb6ddc86cccbc4334
Andy Park <apark1214@gmail.com>
2020-11-16 13:05:32 -0800

    completed javscript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+
+var fullName ="Andy Park";
+var isCool= true;
+var totalPets =1;
+var unknown = null;
+var never =undefined;
+
+console.log('full name:', fullName);
+console.log("type of full name:",typeof fullName);
+console.log('is cool:', isCool);
+console.log("type of is cool:", typeof isCool);
+console.log('total pets:', totalPets);
+console.log("type of total pets:", typeof totalPets);
+console.log('value of null:', unknown);
+console.log('type of null:', typeof unknown);
+console.log('value of never:', never);
+console.log('typeof never', typeof never);
