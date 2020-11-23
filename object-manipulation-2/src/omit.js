772a30af8be4e9fca0dc1b725964cf8d179cde2f
Andy Park <apark1214@gmail.com>
2020-11-23 11:10:04 -0800

    in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -3,14 +3,15 @@
 
 function omit(source, keys) {
 
+  var objectResult ={};
   for (var i = 0; i < keys.length; i++) {
     for (var values in source) {
       if (keys[i] === values) {
         delete source[values]
-
+      }else {
+        objectResult[keys[i]]=source[values];
       }
     }
 
-  }
-  return source;
+  return objectResult;
 }
