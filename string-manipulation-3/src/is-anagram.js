666fc3b8bc636ae982354ac7d8083afb52617263
Andy Park <apark1214@gmail.com>
2020-11-24 09:52:05 -0800

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -15,17 +15,21 @@ function isAnagram(firstString, secondString) {
   for (var j=0;j<secondString.length;j++){
     if(secondStringArray[j]!==" "){
       secondNoSpace.push(secondStringArray[j]);
+
     }
   }
 
-  for (var h=0; h<firstNoSpace.length;h++){
     for (var z=0; z<secondNoSpace.length; z++){
-      if(firstNoSpace[h]===secondNoSpace || secondNoSpace[z] ===firstNoSpace){
-        return true;
-      }else if (firstNoSpace[h]!==secondNoSpace[z] || firstNoSpace.length!==secondNoSpace.length){
-        return false;
+        if(firstNoSpace.includes(secondNoSpace[z]) !==true){
+          return false;
+        }else{
+          return true;
+        }
+
+
       }
-    }
+
+
   }
 
-}
+console.log(isAnagram('roses', 'horse') );
