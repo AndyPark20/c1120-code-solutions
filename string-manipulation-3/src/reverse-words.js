20d5b51de92ed0fc09c78765a9c35e8e204317d6
Andy Park <apark1214@gmail.com>
2020-11-24 23:03:27 -0800

    in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,21 @@
 /* exported reverseWords */
+
+
+function reverseWords(string) {
+
+  var array = string.split('');
+  var reversedStringArray = [];
+  var finalStringResult = '';
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    reversedStringArray.push(string[i]);
+
+  }
+  var finalReversedStringArray = reversedStringArray.join('');
+  var finalSplit = finalReversedStringArray.split(' ');
+  for (var k = finalSplit.length - 1; k > 0; k--) {
+    finalStringResult += finalSplit[k] + ' ';
+
+  }
+  return finalStringResult += finalSplit[0];
+}
