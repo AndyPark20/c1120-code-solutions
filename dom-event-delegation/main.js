d95cbbc5df69e669ed389cbeb2f49b2324c3b008
Andy Park <apark1214@gmail.com>
2020-11-30 11:23:54 -0800

    completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,9 @@
+
+var $taskList =document.querySelector('.task-list');
+
+$taskList.addEventListener('click', function(e){
+  console.log("the value of event.target:", e.target);
+  console.log("the value of event target name:", event.target.tagName);
+  console.log('event target closest element:', e.target.closest('.task-list-item'))
+  e.target.closest('.task-list-item').remove();
+})
