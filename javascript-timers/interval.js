e1c4f3eb36934a4bad2dad8d5545a52d873a93e2
Andy Park <apark1214@gmail.com>
2020-12-09 17:50:46 -0800

    added  to local variable by adding var

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -4,8 +4,8 @@ var number = 4;
 var intervalId =setInterval(countDown,1000)
 
 function countDown(){
+  var $header =document.querySelector('h1');
   number--;
-  $header =document.querySelector('h1');
   $header.textContent = number;
 
 
