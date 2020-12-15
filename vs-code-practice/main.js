84508fc9448e871c648057d17289e6bdeeca4ec9
Andy Park <apark1214@gmail.com>
2020-12-15 12:18:58 -0800

    in progress

--- vs-code-practice/main.js
+++ vs-code-practice/main.js
@@ -1,8 +1,7 @@
 
 // function carInfo(year, make, model, mileage) {
-//   var author ="NDU4MmQ1YTQtNzI5Mi00ZThjLWExZjQtYjU4MmNmNzc3YjFh";
 //   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://api.carmd.com/v3.0/maint?year=' + year +'&make=' + make + '&model='+ model +'&mileage='+ mileage);
+//   xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://api.carmd.com/v3.0/maint?year=' + year + '&make=' + make + '&model=' + model + '&mileage=' + mileage);
 //   xhr.setRequestHeader("content-type", "application/json");
 //   xhr.setRequestHeader("authorization", "Basic NDU4MmQ1YTQtNzI5Mi00ZThjLWExZjQtYjU4MmNmNzc3YjFh");
 //   xhr.setRequestHeader("partner-token", "5228fbdcf1fa422392b0f7ff3226cfbb");
@@ -20,18 +19,18 @@
 
 // }
 
-// carInfo(2015, 'SUBARU', 'FORESTER', 70000)
+// carInfo(2010, 'SUBARU', 'FORESTER', 70000)
 
 
-function recall(){
-  var xhr=new XMLHttpRequest();
-  xhr.open('GET','https://cors-anywhere.herokuapp.com/https://webapi.nhtsa.gov/api/Complaints/vehicle/modelyear/2006/make/bmw/model/325?format=json');
-  xhr.responseType='json';
-  xhr.addEventListener('load',function(){
-    console.log(xhr.status);
-    console.log(xhr.response)
-  })
-xhr.send();
-}
+// function recall(){
+//   var xhr=new XMLHttpRequest();
+//   xhr.open('GET','https://api.codetabs.com/v1/proxy?quest=https://webapi.nhtsa.gov/api/Complaints/vehicle/modelyear/2012/make/bmw/model/m3?format=json');
+//   xhr.responseType='json';
+//   xhr.addEventListener('load',function(){
+//     console.log(xhr.status);
+//     console.log(xhr.response)
+//   })
+// xhr.send();
+// }
 
-recall()
+// recall()
