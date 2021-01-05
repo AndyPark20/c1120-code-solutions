294061b2a653ed7551b28f2d46528ce7559d278b
Andy Park <apark1214@gmail.com>
2021-01-05 13:07:56 -0800

    completed node js challenge

--- node-notes-cli/add.js
+++ node-notes-cli/add.js
@@ -1,10 +1,16 @@
 
 const fs = require('fs');
-const userInput = process.argv;
+const userInput = process.argv[3];
+const dataObject = require('./data.json');
 
-fs.writeFile('./data.json','helllo',(err)=>{
-  if(err){
-    console.log(err)
-  }
+const create = () => {
+  dataObject.notes[dataObject.nextId] = userInput;
+  dataObject.nextId++
+  fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+}
 
-})
+module.exports = create;
--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,6 +1,14 @@
 const read = require('./read')
-
+const create =require('./add');
+const remove = require('./delete');
+const modify = require('./modify');
 
 if(process.argv[2]==='read'){
   read();
+}else if (process.argv[2]==='create'){
+  create();
+}else if (process.argv[2]==='delete'){
+  remove();
+}else if (process.argv[2]==='update'){
+  modify();
 }
