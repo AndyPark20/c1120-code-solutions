8e3b5a79ac29f3b5fc1365f404b1d22dfc4eae40
Andy Park <apark1214@gmail.com>
2021-01-05 10:43:18 -0800

    completed user can read note

---
+++ node-notes-cli/read.js
@@ -0,0 +1,15 @@
+
+const fs = require('fs');
+let object;
+fs.readFile('data.json','utf8',(err,data)=>{
+  if(err){
+    console.log(err)
+  }else if(data){
+    object= JSON.parse(data);
+    const main =object.notes;
+    for (const property in main){
+      console.log(`${property}: ${main[property]}`)
+    }
+
+  }
+})
