f67da6b0622cf3a99965408262db2a74941ba034
Andy Park <apark1214@gmail.com>
2021-01-07 12:59:58 -0800

    revised

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,20 +2,21 @@ const express = require('express');
 const app =express();
 const jsonInitial =express.json();
 let nextId=1;
-const grades={};
-let array=[];
+let grades={};
+
 
 app.use(jsonInitial)
 
 app.get('/api/grades',(req,res,next)=>{
+  let array = [];
+  array.push(grades);
   res.json(array);
 })
 
 app.post('/api/grades',(req,res)=>{
-  let object =req.body;
-  object.id= nextId++;
-  array.push(object)
-  res.status(201).send(array);
+  grades =req.body;
+  grades.id= nextId++;
+  res.status(201).send(grades)
 });
 
 app.listen(3000,()=>{
