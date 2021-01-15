cf347e84bed5d2d8d3d02a371b20598ee010d39f
Andy Park <apark1214@gmail.com>
2021-01-14 16:08:29 -0800

    completed array-map

---
+++ array-map/main.js
@@ -0,0 +1,19 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const doubled = numbers.map(x=>x*2)
+const prices = numbers.map(x=>`$${x.toFixed(2)}`)
+const upperCased = languages.map(x=>x.toUpperCase())
+const firstLetters =languages.map(x=>x.charAt(0))
+
+console.log(`double number value: ${doubled}`)
+console.log(`price fixed ${prices}`)
+console.log(`languages all uppercased: ${upperCased}` )
+console.log(`First letter of the words uppercased: ${firstLetters}`)
