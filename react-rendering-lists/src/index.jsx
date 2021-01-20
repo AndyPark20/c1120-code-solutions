c2cf126711292207c541a311e76a43962d90c9de
Andy Park <apark1214@gmail.com>
2021-01-20 13:27:06 -0800

    practiced via functional component as well.

--- react-rendering-lists/src/index.jsx
+++ react-rendering-lists/src/index.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 
 
+// const pokedex = [
+//   { number: '001', name: 'Bulbasaur' },
+//   { number: '004', name: 'Charmander' },
+//   { number: '007', name: 'Squirtle' },
+//   { number: '025', name: 'Pikachu' },
+//   { number: '039', name: 'Jigglypuff' }
+// ];
+
+
+// export default class RenderPokedex extends React.Component{
+//   render(){
+//     return(
+//       <ul>
+//         <li>{pokedex.map(values=><li key={values.number}>{values.name}</li>)}</li>
+//       </ul>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<RenderPokedex />, document.querySelector('#root'))
+
+/*-------------------------------------------------------------*/
 const pokedex = [
   { number: '001', name: 'Bulbasaur' },
   { number: '004', name: 'Charmander' },
@@ -9,16 +31,16 @@ const pokedex = [
   { number: '025', name: 'Pikachu' },
   { number: '039', name: 'Jigglypuff' }
 ];
-
-
-export default class RenderPokedex extends React.Component{
-  render(){
-    return(
-      <ul>
-        <li>{pokedex.map(values=><li key={values.number}>{values.name}</li>)}</li>
-      </ul>
-    );
-  }
+console.log('testing')
+function RenderPokedex(props){
+  const result =props.list
+  const array=result.map(values=><li key={values.number}>{values.name}</li>)
+  return(
+    <ul>
+      {array}
+    </ul>
+  );
 }
 
-ReactDOM.render(<RenderPokedex />, document.querySelector('#root'))
+const element =<RenderPokedex list={pokedex} />
+ReactDOM.render(element, document.querySelector('#root'));
