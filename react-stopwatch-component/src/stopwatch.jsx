5b2fa43a365695e7224e17e00a685cd4e5f8cf41
Andy Park <apark1214@gmail.com>
2021-01-20 07:45:59 -0800

    completed react-stopwatch-component

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-console.log('testss')
+
 export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ time: 0, actionButton: true});
+    this.state = ({ time: 0, actionButton: true });
     this.autoClock = this.autoClock.bind(this);
     this.screenClick = this.screenClick.bind(this);
   }
 
-  screenClick(){
+  screenClick() {
     console.log('registering!')
-    if(this.state.actionButton)
-    this.setState({time:0})
+    if (this.state.actionButton)
+      this.setState({ time: 0 })
   }
 
   autoClock() {
     const status = this.state.actionButton;
     if (status) {
-      this.setState({actionButton: false })
+      this.setState({ actionButton: false })
       this.intervalId = setInterval(() => {
         this.setState({ time: this.state.time + 1 })
       }, 1000)
     } else {
       clearInterval(this.intervalId);
-      this.setState({actionButton: true })
+      this.setState({ actionButton: true })
     }
 
   }
