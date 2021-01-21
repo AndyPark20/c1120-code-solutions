806dd6790930ae2b8aca8004902a435aa5057fb0
Andy Park <apark1214@gmail.com>
2021-01-21 15:31:57 -0800

    completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -2,57 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const topics = [{
-  id:'html',
-  title: 'Hypertext Markup Language',
-  information: `Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.
-  It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript`
-},
-{
-  id:'css',
-  title: 'Cascading Style Sheets',
-  information: `Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.
-  CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript. `
-},
-{
-  id:'js',
-  title: 'JavaScript',
-  information: `JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.
-JavaScript is high-level, often just-in-time compiled, and multi-paradigm.`
-},
-]
-
-console.log('Will?')
+console.log('test')
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state=({view:'', doubleClick:0});
+    this.state=({view:''});
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event){
-    console.log(this.state)
     const selected =event.target.getAttribute('data-view')
+      this.setState({view:selected})
 
-    if (selected ==='html'){
-      this.setState({view:selected, doubleClick:this.state.doubleClick+1})
-    } else if (selected ==='css'){
-      this.setState({ view: selected, doubleClick: this.state.doubleClick + 1})
-    } else if (selected ==='js'){
-      this.setState({ view: selected, doubleClick: this.state.doubleClick + 1})
-    }
-
+      if(this.state.view === selected){
+        this.setState({view:""})
+      }
   }
 
   render() {
     return (
       <div className="container" onClick={this.handleChange}>
         <div className="row">
-          {topics.map(values=>{
+          {this.props.programming.map(values=>{
             let hide = ''
-            if(this.state.view === values.id && this.state.doubleClick % 2 ===0){
+            if(this.state.view === values.id){
               hide ='';
-            }else if((this.state.view !==values.id) || (this.state.doubleClick % 2 ===1)) {
+            }else{
               hide ='hidden';
             }
             return (<div className="column">
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const topics = [{
+  id: 'html',
+  title: 'Hypertext Markup Language',
+  information: `Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.
+  It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript`
+},
+{
+  id: 'css',
+  title: 'Cascading Style Sheets',
+  information: `Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.
+  CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript. `
+},
+{
+  id: 'js',
+  title: 'JavaScript',
+  information: `JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.
+JavaScript is high-level, often just-in-time compiled, and multi-paradigm.`
+},
+]
 
-ReactDOM.render(<Accordion />,document.querySelector('#root'))
+
+ReactDOM.render(<Accordion programming={topics} />,document.querySelector('#root'))
