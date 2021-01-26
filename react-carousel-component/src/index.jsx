e5687e8d6025e39756090a25128d72193ddd273e
Andy Park <apark1214@gmail.com>
2021-01-26 12:55:17 -0800

    made last revision on deleting dotId

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './carousel';
 
-const dotId=[
-  {id:0},{id:1},{id:2},{id:3},{id:4}
-]
 
-ReactDOM.render(<Carousel pokemon={['./001.png','./004.png','./007.png','./025.png','./039.png']} dotId={dotId} />,document.querySelector('#root'))
+ReactDOM.render(<Carousel pokemon={['./001.png','./004.png','./007.png','./025.png','./039.png']} />,document.querySelector('#root'))
