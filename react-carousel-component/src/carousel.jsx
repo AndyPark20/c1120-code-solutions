8743574bfcc965767a112d208d73c2e15e882ca6
Andy Park <apark1214@gmail.com>
2021-01-25 16:39:11 -0800

    revised

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+console.log('FINAL')
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = ({ counter: 0 })
+    this.state = { counter: 0 }
+    this.handlePreviousImage = this.handlePreviousImage.bind(this)
+    this.handleNextImage = this.handleNextImage.bind(this)
+    this.circleChange = this.circleChange.bind(this)
+    this.circleClick =this.circleClick.bind(this)
+  }
+
+  handlePreviousImage() {
+    if (this.state.counter === 0) {
+      this.setState({ counter: this.props.pokemon.length - 1 })
+    } else {
+      this.setState({ counter: this.state.counter - 1 })
+    }
+  }
+
+  handleNextImage() {
+    if (this.state.counter === this.props.pokemon.length - 1) {
+      this.setState({ counter: 0 })
+    } else {
+      this.setState({ counter: this.state.counter + 1 })
+    }
   }
 
   handleChange(event) {
     const name = event.target.className
     const iconClick = parseInt(event.target.id)
+    if (iconClick === iconClick) {
+      this.setState({ counter: iconClick })
+    }
+  }
+
+  circleChange() {
+    const circle = this.props.pokemon.map((values,index) => {
+      let message =''
+        if(index === this.state.counter){
+          message ="fas fa-circle"
+        }else{
+          message ="far fa-circle"
+        }
+        return <i key={index} className={message} id={index}></i>
+    })
+    return circle;
+  }
+
+  circleClick(event){
+    const idNumber= parseInt(event.target.id)
+    if(typeof idNumber ==='number'){
+      this.setState({counter:idNumber})
+    }
+  }
+
+  pictureChange() {
+    const picture = this.props.pokemon[this.state.counter]
+    return <img src={picture} alt="pokemon" />
+  }
+
+  componentDidMount() {
     clearInterval(this.intervalId)
     this.intervalId = setInterval(() => {
       this.setState({ counter: this.state.counter + 1 })
@@ -19,55 +69,20 @@ export default class Carousel extends React.Component {
         this.setState({ counter: 0 })
       }
     }, 3000)
-    if (name === 'fas fa-chevron-right') {
-      if (this.state.counter === this.props.pokemon.length - 1) {
-        this.setState({ counter: 0 })
-      } else {
-        this.setState({ counter: this.state.counter + 1 })
-      }
-    } else if (name === 'fas fa-chevron-left') {
-      if (this.state.counter === 0) {
-        this.setState({ counter: this.props.pokemon.length - 1 })
-      } else {
-        this.setState({ counter: this.state.counter - 1 })
-      }
-    }
-
-    if (iconClick === iconClick) {
-      this.setState({ counter: iconClick })
-    }
-  }
-
-  circleChange() {
-    const circle = this.props.dotId.map(values => {
-      let message = ''
-      if (this.state.counter === values.id) {
-        message = "fas fa-circle"
-      } else {
-        message = "far fa-circle"
-      }
-      return <i key={values.id} className={message} id={values.id}></i>
-    })
-    return circle;
-  }
-
-  pictureChange() {
-    const picture = this.props.pokemon[this.state.counter]
-    return (<img src={picture} alt="pokemon" />)
   }
 
   render() {
     return (
-      <div className="container" onClick={this.handleChange}>
+      <div className="container" onClick={this.circleChange}>
         <div className="row">
-          <i class="fas fa-chevron-left"></i>
+          <i class="fas fa-chevron-left" onClick={this.handlePreviousImage}></i>
           <div className="column">
             {this.pictureChange()}
-            <div className="circle">
+            <div className="circle" onClick={this.circleClick}>
               {this.circleChange()}
             </div>
           </div>
-          <i class="fas fa-chevron-right"></i>
+          <i class="fas fa-chevron-right" onClick={this.handleNextImage}></i>
         </div>
       </div>
     );
